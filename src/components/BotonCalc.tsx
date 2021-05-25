2045be8b87f39bc80ee3b8b85275c3253c7a5038
Jorge Barcos <jorgebarcos88@gmail.com>
2021-05-25 11:49:57 -0300

    Botón de calculadora y sus filas

---
+++ src/components/BotonCalc.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {styles} from '../theme/appTheme';
+
+interface Props {
+  texto: string;
+  color?: string;
+}
+
+const BotonCalc = ({texto, color = '#2D2D2D'}: Props) => {
+  return (
+    <View style={{...styles.boton, backgroundColor: color}}>
+      <Text style={styles.botonTexto}>{texto}</Text>
+    </View>
+  );
+};
+
+export default BotonCalc;
