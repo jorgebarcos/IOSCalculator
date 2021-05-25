003e45be7edf18af099af036a1ab413fce4c9f8d
Jorge Barcos <jorgebarcos88@gmail.com>
2021-05-25 11:06:51 -0300

    Inicio del proyecto

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hola mundo</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
