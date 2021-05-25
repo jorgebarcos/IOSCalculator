5d93e2ddf72de04deeeb5ed54ad768f23dd75f7e
Jorge Barcos <jorgebarcos88@gmail.com>
2021-05-25 11:20:50 -0300

    Diseño inicial y pantalla de Calculadora

---
+++ src/screens/CalculadoraScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import {styles} from '../theme/appTheme';
+
+const CalculadoraScreen = () => {
+  return (
+    <View>
+      <Text style={styles.texto}>Calculadora Screen</Text>
+    </View>
+  );
+};
+
+export default CalculadoraScreen;
