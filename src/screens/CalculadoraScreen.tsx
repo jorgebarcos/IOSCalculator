bfd6686ba75f9a5273059211c2a1e7bde89a53cb
Jorge Barcos <jorgebarcos88@gmail.com>
2021-05-25 19:19:45 -0300

    Botón borrar última entrada

--- src/screens/CalculadoraScreen.tsx
+++ src/screens/CalculadoraScreen.tsx
@@ -38,6 +38,21 @@ const CalculadoraScreen = () => {
     }
   };
 
+  const btnDelete = () => {
+    let negativo = '';
+    let numeroTemp = numero;
+    if (numero.includes('-')) {
+      negativo = '-';
+      numeroTemp = numero.substr(1);
+    }
+
+    if (numeroTemp.length > 1) {
+      setNumero(negativo + numeroTemp.slice(0, -1));
+    } else {
+      setNumero('0');
+    }
+  };
+
   const positivoNegativo = () => {
     if (numero.includes('-')) {
       setNumero(numero.replace('-', ''));
@@ -57,7 +72,7 @@ const CalculadoraScreen = () => {
       <View style={styles.fila}>
         <BotonCalc texto="C" color="#9B9B9B" accion={limpiar} />
         <BotonCalc texto="+/-" color="#9B9B9B" accion={positivoNegativo} />
-        <BotonCalc texto="del" color="#9B9B9B" accion={limpiar} />
+        <BotonCalc texto="del" color="#9B9B9B" accion={btnDelete} />
         <BotonCalc texto="/" color="#FF9427" accion={limpiar} />
       </View>
 
